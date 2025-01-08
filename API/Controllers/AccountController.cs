8bfe894b12ad0caf54ec7f69c901e6fbe089d1c7
Jamesholloway28 <Jamesholloway28@gmail.com>
2025-01-08 17:57:08 -0500

    just testing

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -18,7 +18,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
 
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
         return Ok();
-        // using var hmac = new HMACSHA512();
+        // using var hmac = new HMACSHA512()
 
         // var user = new AppUser
         // {
@@ -63,3 +63,4 @@ public class AccountController(DataContext context, ITokenService tokenService) 
         return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower()); // Bob != bob
     }
 }
+//test
