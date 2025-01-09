ad5e2e4f64750faff0cb4953134d380cd148ce3d
Jamesholloway28 <Jamesholloway28@gmail.com>
2025-01-08 21:37:20 -0500

    added semi

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -18,7 +18,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
 
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
         return Ok();
-        // using var hmac = new HMACSHA512();;
+        // using var hmac = new HMACSHA512();;;;
 
         // var user = new AppUser
         // {
